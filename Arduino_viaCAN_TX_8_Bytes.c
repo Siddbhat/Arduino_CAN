3252911a99e99e8361e78a036f3ebde67d15edf9
SIDARATH BHAT <137481990+Siddbhat@users.noreply.github.com>
2023-11-03 12:35:14 +0530

    Arduino_viaCAN_TX_8_Bytes

---
+++ Arduino_viaCAN_TX_8_Bytes.c
@@ -0,0 +1,26 @@
+#include <mcp_can.h>
+#include <SPI.h>
+
+MCP_CAN CAN0(10);    
+void setup()
+{
+  Serial.begin(115200);
+
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
+  else Serial.println("Error Initializing MCP2515...");
+
+  CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
+}
+
+byte data[8] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+
+void loop()
+{
+  byte sndStat = CAN0.sendMsgBuf(0x100, 0, 8, data);
+  if(sndStat == CAN_OK){
+    Serial.println("Message Sent Successfully!");
+  } else {
+    Serial.println("Error Sending Message...");
+  }
+  delay(100);   
+}
